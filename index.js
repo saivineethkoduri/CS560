75c359301b7a8c9eccd828974adf2b705e13816c
saivineethkoduri <47078919+saivineethkoduri@users.noreply.github.com>
2022-05-15 23:40:32 -0500

    Delete index.js

--- index.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const users = require("./routes/users")
-const app = express();
-app.listen(8080, () => {
-    console.log("server is running in port  8080")
-
-})
-
-app.use('/', users)
